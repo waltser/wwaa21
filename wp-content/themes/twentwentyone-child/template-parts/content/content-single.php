63442298a65b4c4c18c3dd578563ed33fa2a0a14
Basse <gauthier.basse@interelgroup.com>
2021-03-08 00:20:49 +0100

    Version Live and Nice

--- wp-content/themes/twentwentyone-child/template-parts/content/content-single.php
+++ wp-content/themes/twentwentyone-child/template-parts/content/content-single.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Template part for displaying posts
  *
@@ -18,19 +17,22 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php if (have_rows('fruit')) : ?>
-		<?php while (have_rows('fruit')) : the_row();
-			// Get sub field values.
-			$image = get_sub_field('image'); ?>
-
-		<?php endwhile; ?>
-	<?php endif; ?>
-
-	<div id="womancontent">
-		<?php if (have_rows('fruit')) : ?>
-			<?php while (have_rows('fruit')) : the_row(); ?>
-				<div class="womancontenthead">
+<?php if (have_rows('fruit')) : ?>
+			<?php while (have_rows('fruit')) : the_row();
+				// Get sub field values.
+				$image = get_sub_field('image'); ?>
+				<div id="leftcol">
 					<img class='imagewoman fade-in' src="<?php echo esc_url($image['url']); ?>">
+					<div class="bio">
+							<?php the_field('bio'); ?>
+						</div>
+						</div>
+						<?php endwhile; ?>
+		<?php endif; ?>
+
+						<div id="womancontent">
+					<?php if (have_rows('fruit')) : ?>
+			<?php while (have_rows('fruit')) : the_row();?>
 					<div id="womandetail">
 						<div class="name">
 							<?php the_sub_field('name'); ?>
@@ -38,37 +40,39 @@
 						<div class="job">
 							<?php the_sub_field('job'); ?>
 						</div>
+					</div>
 
-						<div id="contact">
-							<div class="twitter womansm">
-								<a href="<?php the_sub_field('twitter'); ?>"><i class="fab fa-twitter pink"> </i></a>
+					<?php endwhile; ?>
+		<?php endif; ?>
 
-							</div>
-							<div class="linkedin womansm">
-								<a href="<?php the_sub_field('linkedin'); ?>"><i class="fab fa-linkedin pink"> </i></a>
+						
+					<?php if (have_rows('contact')) : ?>
+			<?php while (have_rows('contact')) : the_row();?>
+					
+					<div id="contact">
+						<div class="twitter womansm">
+							<a href="<?php the_sub_field('twitter'); ?>"><i class="fab fa-twitter pink">	</i></a>
+							
+						</div>
+						<div class="linkedin womansm">
+						<a href="<?php the_sub_field('linkedin'); ?>"><i class="fab fa-linkedin pink">	</i></a>
 
+						
+						</div>
+						<div class="towoman womansm">
+						<a href="<?php the_sub_field('mail'); ?>"><i class="fas fa-envelope-open-text pink"></i></a>
 
-							</div>
-							<div class="towoman womansm">
-								<a href="<?php the_sub_field('mail'); ?>"><i class="fas fa-envelope-open-text pink"></i></a>
-
-							</div>
+							
 						</div>
 					</div>
-				</div>
-	</div>
-
-<?php endwhile; ?>
-<?php endif; ?>
-
-<div class="bio">
-	<?php the_field('bio'); ?>
-</div>
-
-<div class="interview">
-	<?php the_field('interview'); ?>
-</div>
-</div>
+					
+			<?php endwhile; ?>
+		<?php endif; ?>
+					
+						<div class="interview">
+							<?php the_field('interview'); ?>
+						</div>
+					</div>
 
 
 
